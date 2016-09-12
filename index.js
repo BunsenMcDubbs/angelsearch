98976c1e2ac5841cde602b47982049846e117c2e
Andrew Dai <theman@andrewdai.co>
2016-09-12 14:48:36 -0400

    add html and js client

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use('/', express.static('public/views'));
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-      res.send('Hello World!');
+  res.send("hi");
 });
 
 app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
